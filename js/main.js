eed0fe58372740000954c65ebc3e010e90aa7bdf
Sinthex91 <49324747+Sinthex91@users.noreply.github.com>
2020-01-21 21:51:59 +0100

    Update Text
    Texte bearbeitet + Intro

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var text, eingabe;
 var laenge = 3;
 var zaehler = 0;
 var ausgabe = "";
-
+alert("Wiederhole die Ziffernfolge, so lange du kannst!");
 do {
   zaehler++;
   if (zaehler > 3) {
@@ -14,12 +14,12 @@ do {
   for (let i = 1; i <= laenge; i++) text += Math.floor(Math.random() * 10);
   alert("Ziffernfolge: " + text);
 
-  eingabe = prompt("Ihre Eingabe");
+  eingabe = prompt("Deine Eingabe");
   ausgabe += text + "<br>";
 } while (eingabe == text);
 
 //Bewertung
 var ergebnis = laenge - 1;
 if (ergebnis < 3) ergebnis = 0;
-ausgabe += "Sie konnten sich " + ergebnis + " Ziffern merken";
+ausgabe += "Du konntest dir " + ergebnis + " Ziffern merken";
 document.write(ausgabe);
